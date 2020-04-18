080a4dfc4b289974e0b4fe0730a21e6803184dca
Paulo <paulosanz@poli.ufrj.br>
2020-04-18 17:39:19 -0300

    Login to gnosys

--- main.py
+++ main.py
@@ -3,32 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 
 def login_to_siga(user, password):
-    cas_url = "https://cas.ufrj.br/login"
-    cas_get_resp = requests.get(cas_url)
+    with requests.Session() as session:
+        auxiliar_url = "https://intranetauxiliar.ufrj.br/LoginUfrj/redireciona/?url_redir=https%3A%2F%2Fsiga.ufrj.br%2Fsira%2Fintranet%2FLoginIntranet.jsp%3FidentificacaoUFRJ%3D%3Aidentificacao_ufrj%3A%26idSessao%3D%3Aid_sessao%3A"
+        auxiliar_resp = session.get(auxiliar_url)
 
-    soup = BeautifulSoup(cas_get_resp.content.decode('utf-8'), "html.parser")
-    csrf_token = soup.find("meta", {"name": "csrf-token"})["content"]
-    lt = soup.find("input", {"id": "lt"})["value"]
+        soup = BeautifulSoup(auxiliar_resp.content.decode('utf-8'), "html.parser")
+        authenticity_token = soup.find("input", {"name": "authenticity_token"})["value"]
+        lt = soup.find("input", {"id": "lt"})["value"]
 
-    cas_post_resp = requests.post(cas_url, data = {
-        "authenticity_token": csrf_token,
-        "lt": lt,
-        "service": "https://intranet.ufrj.br/cas?destination=node/4",
-        "username": user,
-        "password": password
-    })
+        cas_resp = session.post(auxiliar_resp.url, data = {
+            "authenticity_token": authenticity_token,
+            "lt": lt,
+            "username": user,
+            "password": password
+        })
 
-    print(cas_post_resp.content.decode("utf-8"))
-    print("\n\n\n\n\n")
-    print("You are logged in")
-    #soup = BeautifulSoup(cas_post_resp.content.decode('utf-8'), "html.parser")
-    #next_url = soup.find("div", {"id": "block-block-3"}).find("div", {"class": "content"}).find_all("p")[3].find("a")["href"]
+        gnosys_url = "https://gnosys.ufrj.br/Portal/auth.seam"
+        gnosys_resp = session.get(gnosys_url)
+        soup = BeautifulSoup(gnosys_resp.content.decode("utf-8"), "html.parser")
+        name = soup.find("div", {"class":"gnosys-login-nome"}).text
+        print(title_case(name))
+        return (gnosys_url, gnosys_resp.cookies)
 
-    #auxiliar = requests.get(next_url)
-    #print(auxiliar.content.decode("utf-8"))
+def title_case(txt):
+    return " ".join([t[0].upper() + t[1:].lower() for t in txt.split(" ")])
 
 if __name__ == "__main__":
     if len(argv) != 3:
         print("Must call script with username and password\n  python3 main.py <username> <password>")
     else:
-        login_to_siga(argv[1], argv[2])
+        print(login_to_siga(argv[1], argv[2]))
