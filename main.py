7d9e6577380ce819caaeeedcbe3e865167e868c0
Paulo <paulosanz@poli.ufrj.br>
2020-04-18 21:52:17 -0300

    Split PR #1 in functions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sys import argv
 from bs4 import BeautifulSoup
 import requests
 
+portal_uri = "https://gnosys.ufrj.br"
+
 def login_to_siga(user, password):
     with requests.Session() as session:
         auxiliar_url = "https://intranetauxiliar.ufrj.br/LoginUfrj/redireciona/?url_redir=https%3A%2F%2Fsiga.ufrj.br%2Fsira%2Fintranet%2FLoginIntranet.jsp%3FidentificacaoUFRJ%3D%3Aidentificacao_ufrj%3A%26idSessao%3D%3Aid_sessao%3A"
@@ -18,12 +20,27 @@ def login_to_siga(user, password):
             "password": password
         })
 
-        gnosys_url = "https://gnosys.ufrj.br/Portal/auth.seam"
-        gnosys_resp = session.get(gnosys_url)
-        soup = BeautifulSoup(gnosys_resp.content.decode("utf-8"), "html.parser")
-        name = soup.find("div", {"class":"gnosys-login-nome"}).text
-        print(title_case(name))
-        return (gnosys_url, gnosys_resp.cookies)
+        gnosys_resp = session.get(portal_uri + "/Portal/auth.seam")
+        return gnosys_resp.cookies
+
+def personal_info(cookies):
+    with requests.Session() as session:
+        session.cookies = cookies
+        session.get(portal_uri + "/Registro")
+        resp = session.get(portal_uri + "/Registro/auth.seam")
+        soup = BeautifulSoup(resp.content.decode("utf-8"), "html.parser")
+        return str(soup.find("div", {"id": "blocoDadosPessoais"}))
+
+def enrolled_info(cookies):
+    with requests.Session() as session:
+        session.cookies = cookies
+        session.get(portal_uri + "/Inscricao")
+        resp = session.get(portal_uri + "/Inscricao/auth.seam")
+        soup = BeautifulSoup(resp.content.decode("utf-8"), "html.parser")
+        token = soup.find("span", {"id": "token"}).text
+
+        resp = session.get(portal_uri + "/Inscricao/seam/resource/rest/inscricao/pedidos?token=" + token)
+        return resp.content.decode("utf-8")
 
 def title_case(txt):
     return " ".join([t[0].upper() + t[1:].lower() for t in txt.split(" ")])
@@ -32,4 +49,6 @@ if __name__ == "__main__":
     if len(argv) != 3:
         print("Must call script with username and password\n  python3 main.py <username> <password>")
     else:
-        print(login_to_siga(argv[1], argv[2]))
+        cookie = login_to_siga(argv[1], argv[2])
+        print(personal_info(cookie))
+        print(enrolled_info(cookie))
